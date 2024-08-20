80f3c0752d63ad6723d54d651054a4613b31e9f9
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-08-20 21:19:23 +0530

    Add basic monkey objects

---
+++ monkey/src/object.ts
@@ -0,0 +1,23 @@
+export enum MonkeyObjectType {
+  INT = 'INT',
+  BOOL = 'BOOL',
+  NULL = 'NULL',
+}
+
+export abstract class MonkeyObject {
+  abstract type: MonkeyObjectType
+}
+
+export class Null implements MonkeyObject {
+  type: MonkeyObjectType = MonkeyObjectType.NULL
+}
+
+export class Int implements MonkeyObject {
+  type: MonkeyObjectType = MonkeyObjectType.INT
+  constructor(public value: number) {}
+}
+
+export class Bool implements MonkeyObject {
+  type: MonkeyObjectType = MonkeyObjectType.BOOL
+  constructor(public value: number) {}
+}
