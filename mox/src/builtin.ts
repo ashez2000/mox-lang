9a599915229c8c393d0333f2012ec0390f634a97
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-08-22 20:46:42 +0530

    Use performance.now for clock

--- mox/src/builtin.ts
+++ mox/src/builtin.ts
@@ -1,5 +1,7 @@
+import { performance } from 'perf_hooks'
+
 import * as object from './object'
 
-const clock = new object.Builtin(() => new object.Int(new Date().getMilliseconds()))
+const clock = new object.Builtin(() => new object.Int(performance.now()))
 
 export default new Map<string, object.Builtin>([['clock', clock]])
