a027fa8d63c7efb8185165a88b107189c21fd221
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-08-23 16:26:48 +0530

    Wire up the interpreter to web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,57 @@
 import { useState } from 'react'
 import run from './interpreter'
 
+const example = `\
+let fib = fn (n) {
+    if (n < 2) { return n; };
+    return fib(n - 1) + fib(n - 2);
+}
+
+print fib(6);
+`
+
 export default function App() {
-  const [input, setInput] = useState('')
-  const [result, setResult] = useState('')
+  const [input, setInput] = useState(example)
+  const [output, setOutput] = useState<string[]>([])
+  const [errors, setErrors] = useState<string[]>([])
 
   const evaluate = () => {
-    setResult(run(input))
+    const result = run(input)
+    setOutput(result.output)
+    setErrors(result.errors)
   }
 
   return (
-    <main className="max-w-7xl mx-auto px-3">
-      <header>
+    <main className="max-w-7xl mx-auto p-3 font-mono">
+      <header className="mb-3">
         <h1 className="text-4xl">Mox Lang v0.1.0</h1>
       </header>
-      <div>
-        <button onClick={evaluate}>Evaluate</button>
+
+      <div className="mb-3">
+        <button className="px-3 py-2 bg-zinc-200" onClick={evaluate}>
+          Evaluate
+        </button>
       </div>
-      <div>
-        <textarea value={input} onChange={(e) => setInput(e.target.value)} />
-        <div>{result}</div>
+
+      <div className="grid grid-cols-2 gap-3">
+        <textarea
+          className="p-3 border border-zinc-800 rounded outline-none"
+          style={{ height: '80vh' }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div className="p-3 border border-zinc-400 rounded">
+          <div>
+            {errors.map((e) => (
+              <div className="text-red-600">{e}</div>
+            ))}
+          </div>
+          <div>
+            {output.map((e) => (
+              <div>{e}</div>
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   )
--- web/src/interpreter.ts
+++ web/src/interpreter.ts
@@ -1,12 +1,18 @@
 import { Lexer, Parser, Interpreter } from 'mox'
 
-export default function run(input: string): string {
+export default function run(input: string) {
+  const output: string[] = []
+  const errors: string[] = []
+
   const lexer = Lexer.new(input)
   const parser = Parser.new(lexer)
   const program = parser.parse()
+  for (const e of parser.errors) {
+    errors.push(e)
+  }
 
-  const interpreter = new Interpreter()
-  const result = interpreter.interpret(program)
+  const interpreter = new Interpreter((value) => output.push(value))
+  interpreter.interpret(program)
 
-  return result.toString()
+  return { output, errors }
 }
