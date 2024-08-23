2f940bd48af0ae99e031e203eec18a0bbbc4db73
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-08-23 15:16:20 +0530

    Setup react app

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="max-w-7xl mx-auto px-3">
+      <header>
+        <h1 className="text-4xl">Mox Lang v0.1.0</h1>
+      </header>
+    </main>
+  )
+}
