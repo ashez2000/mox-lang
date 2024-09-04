4960c25c219cabbfc2aa30782773041ec99a0e81
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-09-04 11:17:51 +0530

    Add inbuilt fn len

--- mox/src/builtin.ts
+++ mox/src/builtin.ts
@@ -2,4 +2,25 @@ import * as object from './object'
 
 const clock = new object.Builtin(() => new object.Int(performance.now()))
 
-export default new Map<string, object.Builtin>([['clock', clock]])
+const len = new object.Builtin((...args) => {
+  if (args.length != 1) {
+    return new object.Error('wrong number of arguments')
+  }
+
+  const t = args[0]
+
+  if (t instanceof object.String) {
+    return new object.Int(t.value.length)
+  }
+
+  if (t instanceof object.Array) {
+    return new object.Int(t.elements.length)
+  }
+
+  return new object.Error('len not supported for type ' + t.type)
+})
+
+export default new Map<string, object.Builtin>([
+  ['clock', clock],
+  ['len', len],
+])
