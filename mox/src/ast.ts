bc395392dc6eb3af9876dbb33c54dd40b2c4d5da
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-10-06 23:09:35 +0530

    Add new Ast structs

---
+++ mox/src/ast.ts
@@ -0,0 +1,39 @@
+import { Token } from './token.js'
+
+export interface Ast {
+  type: AstType
+}
+
+export interface Statement extends Ast {}
+
+export interface Expression extends Ast {}
+
+export enum AstType {
+  Program = 0,
+  Let,
+  Ident,
+}
+
+/**
+ * Program Ast
+ */
+export class Program implements Ast {
+  public type: AstType = AstType.Program
+  constructor(public statements: Statement[]) {}
+}
+
+/**
+ * Let Ast
+ */
+export class Let implements Statement {
+  public type: AstType = AstType.Let
+  constructor(public ident: Ident, public expr: Expression) {}
+}
+
+/**
+ * Ident Ast
+ */
+export class Ident implements Expression {
+  public type: AstType = AstType.Ident
+  constructor(public name: Token) {}
+}
