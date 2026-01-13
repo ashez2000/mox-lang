91aae9970c50f8d286bbdf37178a27b86dcb3977
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2026-01-13 20:46:32 +0530

    Fix precedence

--- mox/src/precedence.ts
+++ mox/src/precedence.ts
@@ -1,25 +1,25 @@
 import { TokenType } from './token.js'
 
 export enum Precedence {
-  LOWEST = 0,
-  EQUALS,
-  LTGT,
-  SUM,
-  PRODUCT,
-  PREFIX,
-  CALL,
-  INDEX,
+  Lowest = 0,
+  Equals,
+  LessGreater,
+  Sum,
+  Product,
+  Prefix,
+  Call,
+  Index,
 }
 
 export const precedences = new Map<TokenType, Precedence>([
-  [TokenType.EQUAL, Precedence.EQUALS],
-  [TokenType.NOT_EQUAL, Precedence.EQUALS],
-  [TokenType.LESS_THAN, Precedence.LTGT],
-  [TokenType.GREATER_THAN, Precedence.LTGT],
-  [TokenType.PLUS, Precedence.SUM],
-  [TokenType.MINUS, Precedence.SUM],
-  [TokenType.SLASH, Precedence.PRODUCT],
-  [TokenType.ASTERISK, Precedence.PRODUCT],
-  [TokenType.LEFT_PAREN, Precedence.CALL],
-  [TokenType.LEFT_BRACE, Precedence.INDEX],
+  [TokenType.Eq, Precedence.Equals],
+  [TokenType.NotEq, Precedence.Equals],
+  [TokenType.Lt, Precedence.LessGreater],
+  [TokenType.Gt, Precedence.LessGreater],
+  [TokenType.Plus, Precedence.Sum],
+  [TokenType.Minus, Precedence.Sum],
+  [TokenType.Slash, Precedence.Product],
+  [TokenType.Asterisk, Precedence.Product],
+  [TokenType.LParen, Precedence.Call],
+  [TokenType.LBracket, Precedence.Index],
 ])
