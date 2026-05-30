980c3ffc70356a7c1569eecfd9bd354934349da7
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2026-05-30 16:42:35 +0530

    Add new ast structure

---
+++ mox/src/ast.ts
@@ -0,0 +1,47 @@
+import { Token } from './token.js'
+
+export type Ast = Stmt | Expr
+
+export const enum AstType {
+  LetStmt = 'LetStmt',
+  ReturnStmt = 'ReturnStmt',
+  ExprStmt = 'ExprStmt',
+  PrintStmt = 'PrintStmt',
+
+  Null = 'Null',
+  Bool = 'Bool',
+  Int = 'Int',
+  String = 'String',
+  Ident = 'Ident',
+  Prefix = 'Prefix',
+  Infix = 'Infix',
+  If = 'If',
+  Function = 'Function',
+  Call = 'Call',
+  Array = 'Array',
+  Index = 'Index',
+  HashMap = 'HashMap',
+}
+
+export type Ident = { type: AstType.Ident; name: Token }
+
+export type Stmt =
+  | { type: AstType.LetStmt; name: Token; value: Expr }
+  | { type: AstType.ReturnStmt; keyword: Token; expr: Expr }
+  | { type: AstType.ExprStmt; expr: Expr }
+  | { type: AstType.PrintStmt; expr: Expr }
+
+export type Expr =
+  | { type: AstType.Null }
+  | { type: AstType.Bool; value: boolean }
+  | { type: AstType.Int; value: number }
+  | { type: AstType.String; value: string }
+  | Ident
+  | { type: AstType.Prefix; op: Token; expr: Expr }
+  | { type: AstType.Infix; op: Token; left: Expr; right: Expr }
+  | { type: AstType.If; condition: Expr; thenBlock: Stmt[]; elseBlock?: Stmt[] }
+  | { type: AstType.Function; parameters: Ident[]; body: Stmt[] }
+  | { type: AstType.Call; fnExpr: Expr; args: Expr[] }
+  | { type: AstType.Array; elements: Expr[] }
+  | { type: AstType.Index; left: Expr; index: Expr }
+  | { type: AstType.HashMap; keys: Expr[]; values: Expr[] }
